44c4b76e8a10b5ee355756e41c6113e7ee2ec9c6
G00410156@atu.ie <g00410156@atu.ie>
2023-11-15 14:33:52 +0000

    FIrst Commit

--- src/main/java/ie/atu/week7application/Person.java
+++ src/main/java/ie/atu/week7application/Person.java
@@ -1,4 +1,16 @@
 package ie.atu.week7application;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Person {
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String name;
+
+
 }
