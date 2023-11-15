0486248196eb76ea7b86578f3b5667fef51bad8a
G00410156@atu.ie <g00410156@atu.ie>
2023-11-15 14:26:47 +0000

    FIrst Commit

---
+++ src/main/java/ie/atu/week7application/Week7Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week7application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week7Application.class, args);
+    }
+
+}
